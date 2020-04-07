665522694ea8032d7465f9c3d74905da41a26c73
Dan DeBlasio <dan@dandeblasio.com>
2020-04-07 09:09:57 -0500

    Removing blank file

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
-javac -source 1.5 -target 1.5 facet/*.java opal/*.java opal/*/*.java opal/*/*/*.java
+javac facet/*.java opal/*.java opal/*/*.java opal/*/*/*.java
 exit;
 
 cd facet
-javac -source 1.5 *.java
+javac *.java
 cd ..
 
 cd opal
